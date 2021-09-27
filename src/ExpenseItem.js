e172970cd77065a28f54a2e8c42fcc4a49f5f4ba
manodeeban <you@example.com>
2021-09-27 09:07:18 +0530

    first commit

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState }from "react";
 import "./ExpenseItem.css";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate.js";
 
+
 function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem Evaluated by React");
+
+ 
+
+  const ClickHandler = () => {
+    setTitle("update");
+    console.log(title);
+  };
+
+  const click = () => {
+    alert("amount found!!!");
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+
+        <button onClick={click}>
+          <div className="expense-item__price">${props.amount}</div>
+        </button>
+        <button onClick={ClickHandler}>click me</button>
       </div>
-    </div>
+    </Card>
   );
 }
 
